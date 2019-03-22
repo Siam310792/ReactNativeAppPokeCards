39702f8a1ed3739877a295264e4b5fe7180db505
Annabelle <annabelle.braye@gmail.com>
2019-03-22 15:48:13 +0100

    Display name of the pokeon on click

--- src/components/PokemonListItem.js
+++ src/components/PokemonListItem.js
@@ -1,20 +1,38 @@
 import React from "react";
-import { Image } from "react-native";
+import { Image, TouchableOpacity, Alert } from "react-native";
 
-function PokemonListItem({ card }) {
-  return (
-    <Image
-      style={{
-        height: 200,
-        width: 150,
-        flexDirection: "row",
-        justifyContent: "space-around",
-        alignItems: "flex-start",
-        alignContent: "space-around"
-      }}
-      source={{ uri: card.imageUrl }}
-    />
-  );
+class PokemonListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      card: props.card
+    };
+  }
+
+  _onPressButton() {
+    Alert.alert(this.state.card.name);
+  }
+
+  render() {
+    return (
+      <TouchableOpacity
+        onPress={this._onPressButton.bind(this)}
+        underlayColor="white"
+      >
+        <Image
+          style={{
+            height: 200,
+            width: 150,
+            flexDirection: "row",
+            justifyContent: "space-around",
+            alignItems: "flex-start",
+            alignContent: "space-around"
+          }}
+          source={{ uri: this.state.card.imageUrl }}
+        />
+      </TouchableOpacity>
+    );
+  }
 }
 
 export default PokemonListItem;
